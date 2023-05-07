def13e776f6eb1c6397b7be0139eeaf4c6f858bb
patry <patry@PSUKIENNIK_LAPT>
2023-05-07 16:50:20 +0200

    Dodaj pliki projektów.

---
+++ PaiTwitterApi/Controllers/UsersController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PaiTwitterApi.Models;
+
+namespace PaiTwitterApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        private readonly PaiTwitterContext _context;
+
+
+        public UsersController(PaiTwitterContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TUser>>> GetUsers()
+        {
+            return await _context.TUser.ToListAsync();
+        }
+
+        // GET: Users/Details/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TUser>> GetUsers(int? id)
+        {
+            
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tUser = await _context.TUser
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (tUser == null)
+            {
+                return NotFound();
+            }
+
+            tUser.FirstName += "  " + User.Claims.Where(c => c.Type == "Id").First().Value;
+
+            return tUser;
+        }
+
+        // POST: Users
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,FirstName,LastName,UserName,Email,Password,CreatedDate,LastActivity")] TUser tUser)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tUser);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditUser(int id, TUser TUser)
+        {
+            if (id != TUser.UserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(TUser).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TUserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tUser = await _context.TUser
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (tUser == null)
+            {
+                return NotFound();
+            }
+
+            _context.TUser.Remove(tUser);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        private bool TUserExists(int id)
+        {
+            return _context.TUser.Any(e => e.UserId == id);
+        }
+    }
+}
---
+++ PaiTwitterApi/Models/PaiTwitterContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace PaiTwitterApi.Models
+{
+    public partial class PaiTwitterContext : DbContext
+    {
+
+        public PaiTwitterContext(DbContextOptions<PaiTwitterContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TUser> TUser { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TUser>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__tUser__1788CC4C65F92F39");
+
+                entity.ToTable("tUser");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastActivity).HasColumnType("datetime");
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
